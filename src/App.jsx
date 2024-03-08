8466bf3f32e144db9eaab2b74967aed0f30dc81f
shanetoken <158006619+shanetoken@users.noreply.github.com>
2024-03-08 15:31:59 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+
+const sound = new Audio('https://www.sousound.com/music/jazz_fusion/jazz_01.mp3');
+
+function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 100);
+      }, 100);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]); // Only update effect when isRunning changes
+
+  const formattedTime = () => {
+    const minutes = Math.floor((elapsedTime % (60 * 60 * 1000)) / (60 * 1000));
+    const seconds = Math.floor((elapsedTime % (60 * 1000)) / 1000);
+    const miliseconds = Math.floor(elapsedTime % 1000);
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${miliseconds.toString().padStart(3, '0')}`;
+  };
+
+  useEffect(() => {
+    if (Math.floor((elapsedTime % (60 * 60 * 1000)) / (60 * 1000)) >= 30 && isRunning) {
+      sound.play().catch((error) => {
+        console.error("Error playing sound:", error);
+      });
+    }
+  }, [elapsedTime, isRunning]); // Update when elapsedTime or isRunning changes
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    sound.pause(); // Pause sound on stop
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    sound.currentTime = 0; // Reset sound position on reset
+  };
+
+  return (
+    <>
+      <div className="stopwatch">
+        <div id="stopwatch">{formattedTime()}</div>
+        <div id="text-stop-watch">M : S : ML</div>
+      </div>
+      <div className="btn-watch">
+        <button onClick={handleStart} disabled={isRunning}>Start</button>
+        <button onClick={handleStop} disabled={!isRunning}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
